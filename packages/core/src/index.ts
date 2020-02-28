b87bd0af88794f05fdf86940e85155a6e1401845
Mythie <me@lucasjamessmith.me>
2020-02-29 03:05:43 +1100

    fix: load config during app intialization

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,16 @@
+// We need to disable eslint ordering for this file since
+// we require dotenv to load before everything else
+/* eslint-disable import/order,import/first */
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import 'reflect-metadata';
+
+import { getConfig } from './config';
 import { connect } from './database';
 import { start } from './server';
+
+getConfig();
 
 export default connect().then(() => start());
