264bf78f86e37fc446a4cc7c8649ea47fff97122
Mythie <me@lucasjamessmith.me>
2020-02-29 03:08:03 +1100

    feat: create initial release-bot

---
+++ packages/core/src/workers/release-bot.ts
@@ -0,0 +1,41 @@
+import { Connection, LessThan } from 'typeorm';
+
+import { connect } from '../database';
+import { Release, Document } from '../entities';
+import { ONE_MINUTE } from '../common/constants/base';
+
+let connection: Connection | null = null;
+
+const main = async (): Promise<void> => {
+  if (!connection) {
+    connection = await connect();
+  }
+
+  const releaseRepository = connection.getRepository(Release);
+  const documentRepository = connection.getRepository(Document);
+
+  const releases = await releaseRepository.find({
+    where: {
+      scheduledFor: LessThan(new Date()),
+      publishedAt: null,
+    },
+    relations: ['documents'],
+  });
+
+  releases.forEach(async release => {
+    /* eslint-disable no-param-reassign */
+    const publishPromises = release.documents.map(doc => {
+      doc.publishedAt = new Date();
+      doc.updatedAt = new Date();
+      doc.releaseId = undefined;
+
+      return documentRepository.save(doc);
+    });
+
+    await Promise.all(publishPromises);
+  });
+
+  setTimeout(() => main(), ONE_MINUTE);
+};
+
+main();
