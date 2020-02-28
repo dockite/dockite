a58cf938bd4c889f2a4cb28c45d92358bcc3044d
Mythie <me@lucasjamessmith.me>
2020-02-29 03:07:21 +1100

    feat: add loading of external entities from a config file

--- packages/core/src/database.ts
+++ packages/core/src/database.ts
@@ -1,5 +1,6 @@
 import { Connection, createConnection } from 'typeorm';
 
+import { getConfig } from './config';
 import * as entities from './entities';
 import { getenv } from './utils';
 
@@ -7,7 +8,15 @@ const useSSL = (): boolean => {
   return getenv('PG_SSL', 'false').toLowerCase() === 'true';
 };
 
+const config = getConfig();
+
 export const connect = async (): Promise<Connection> => {
+  let externalEntities: any[] = []; // eslint-disable-line
+
+  if (config.entities && config.entities.length > 0) {
+    externalEntities = await Promise.all(config.entities.map(x => import(x)));
+  }
+
   const connection = await createConnection({
     type: 'postgres',
     host: getenv('PG_HOST', 'localhost'),
@@ -16,7 +25,7 @@ export const connect = async (): Promise<Connection> => {
     database: getenv('PG_DATABASE', 'dockite'),
     ssl: useSSL(),
     synchronize: true,
-    entities: Object.values(entities),
+    entities: [...Object.values(entities), ...externalEntities],
   });
 
   return connection;
