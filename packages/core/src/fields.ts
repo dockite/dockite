85e8be3c51009f8e3be95e4a6154282cf1e52d5c
Mythie <me@lucasjamessmith.me>
2020-03-12 08:32:30 +1100

    feat(core): add base fields manager

---
+++ packages/core/src/fields.ts
@@ -0,0 +1,19 @@
+import { DockiteFieldStatic } from '@dockite/field';
+import debug from 'debug';
+
+const log = debug('prime:core:fields');
+
+export interface DockiteFieldManager {
+  [id: string]: DockiteFieldStatic;
+}
+
+export const dockiteFields: DockiteFieldManager = {};
+
+export const registerField = (id: string, field: any): void => {
+  if (!dockiteFields[id]) {
+    log(`registering field ${id}`);
+    dockiteFields[id] = field;
+  } else {
+    log(`field ${id} already exists`);
+  }
+};
