d7e096bd2646ca02d75df94319c81c47a1d23cb0
Mythie <me@lucasjamessmith.me>
2020-03-25 08:34:29 +1100

    feat(field-string): add ui for custom field

---
+++ packages/field-string/src/ui/Input.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-input :defaultValue="fieldData" size="large" />
+</template>
+
+<script>
+import Vue from 'vue';
+
+import Input from 'ant-design-vue/lib/input';
+import 'ant-design-vue/lib/input/css';
+
+Vue.use(Input);
+
+export default {
+  name: 'StringField',
+  props: {
+    fieldData: {
+      required: true,
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    nice() {
+      return 'nice'
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ packages/field-string/src/ui/index.ts
@@ -0,0 +1,5 @@
+import { registerField } from '@dockite/field';
+
+import Input from './Input.vue';
+
+export default registerField('string', Input, Input);
