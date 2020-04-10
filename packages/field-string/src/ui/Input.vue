f27c36a3976a88370b0c607fbf2281d15863dc2d
Mythie <me@lucasjamessmith.me>
2020-04-10 11:57:51 +1000

    fix(field-string): update string field to include settings

--- packages/field-string/src/ui/Input.vue
+++ packages/field-string/src/ui/Input.vue
@@ -1,19 +1,40 @@
 <template>
-  <a-form-item :label="fieldConfig.title" :colon="true">
-    <a-input v-model="fieldData" size="large" />
+  <a-form-model-item
+    :label="fieldConfig.title"
+    :colon="true"
+    :prop="fieldConfig.name"
+  >
+    <a-input
+      v-if="!fieldConfig.settings.textarea"
+      v-model="fieldData"
+      size="large"
+    />
+    <a-textarea
+      v-if="fieldConfig.settings.textarea"
+      v-model="fieldData"
+      size="large"
+      :auto-size="{ minRows: 3, maxRows: 5 }"
+      :allow-clear="true"
+    />
     <p slot="extra">
       {{ fieldConfig.description }}
     </p>
-  </a-form-item>
+  </a-form-model-item>
 </template>
 
 <script>
+import isURL from 'validator/es/lib/isURL';
+
 export default {
   name: 'StringField',
 
   props: {
+    name: {
+      type: String,
+      required: true,
+    },
     value: {
-      validator: value => typeof value === 'string' || value === null,
+      validator: (value) => typeof value === 'string' || value === null,
       required: true,
     },
     formData: {
@@ -49,6 +70,55 @@ export default {
     if (this.value === null) {
       this.$emit('input', '');
     }
+
+    const rules = [];
+
+    if (this.fieldConfig.settings.required) rules.push(this.getRequiredRule());
+    if (this.fieldConfig.settings.urlSafe) rules.push(this.getUrlSafeRule());
+    if (this.fieldConfig.settings.minLen) rules.push(this.getMinRule());
+    if (this.fieldConfig.settings.maxLen) rules.push(this.getMaxRule());
+
+    this.$emit('update:rules', { [this.fieldConfig.name]: rules });
+  },
+
+  methods: {
+    getUrlSafeRule() {
+      return {
+        validator(_rule, value, callback) {
+          if (!isURL(value)) {
+            return callback(false);
+          }
+
+          return callback();
+        },
+        message: `${this.fieldConfig.title} must be URL Safe`,
+        trigger: 'change',
+      };
+    },
+
+    getRequiredRule() {
+      return {
+        required: true,
+        message: `${this.fieldConfig.title} is required`,
+        trigger: 'change',
+      };
+    },
+
+    getMinRule() {
+      return {
+        min: Number(this.fieldConfig.settings.minLen),
+        message: `${this.fieldConfig.title} must contain atleast ${this.fieldConfig.settings.minLen} characters.`,
+        trigger: 'change',
+      };
+    },
+
+    getMaxRule() {
+      return {
+        max: Number(this.fieldConfig.settings.maxLen),
+        message: `${this.fieldConfig.title} must contain no more than ${this.fieldConfig.settings.maxLen} characters.`,
+        trigger: 'change',
+      };
+    },
   },
 };
 </script>
