65ac67482d765b273cd3c39e0365766629d8cbd0
Mythie <me@lucasjamessmith.me>
2020-04-18 23:32:49 +1000

    feat(field-reference): create the reference field
    The reference field allows you to reference another document from a schema. When the reference is queried the document it references will be resolved and returned allowing you to obtain it's data as well.
    
    Still pending is complexity constraints so and disallowing self-referential documents.

---
+++ packages/field-reference/src/index.ts
@@ -0,0 +1,76 @@
+import { DockiteField } from '@dockite/field';
+import { Document, Schema } from '@dockite/types';
+import {
+  GraphQLInputFieldConfigMap,
+  GraphQLInputObjectType,
+  GraphQLInputType,
+  GraphQLObjectType,
+  GraphQLOutputType,
+  GraphQLString,
+  GraphQLUnionType,
+} from 'graphql';
+
+export class DockiteFieldReference extends DockiteField {
+  public static type = 'reference';
+
+  public static title = 'Reference';
+
+  public static description = 'A reference field';
+
+  public static defaultOptions = {};
+
+  public async inputType(): Promise<GraphQLInputType> {
+    return new GraphQLInputObjectType({
+      name: 'ReferenceFieldInput',
+      fields: {
+        id: { type: GraphQLString },
+        schemaId: { type: GraphQLString },
+      } as GraphQLInputFieldConfigMap,
+    });
+  }
+
+  // public async processInput<Input, Output>(data: Input): Promise<Output> {}
+
+  public async where(): Promise<GraphQLInputType> {
+    return GraphQLString;
+  }
+
+  public async outputType(
+    dockiteSchemas: Schema[],
+    types: Map<string, GraphQLObjectType>,
+  ): Promise<GraphQLOutputType> {
+    const schemaIds: string[] = this.schemaField.settings.schemaIds ?? [];
+    // const schemaIds = this
+
+    const unionTypes = dockiteSchemas
+      .filter(schema => schemaIds.includes(schema.id))
+      .map(schema => types.get(schema.name));
+
+    if (unionTypes.length === 1) {
+      const [outputType] = unionTypes;
+
+      return outputType as GraphQLObjectType;
+    }
+
+    return new GraphQLUnionType({
+      name: `${this.schemaField.name}_Union`,
+      types: unionTypes as GraphQLObjectType[],
+    });
+  }
+
+  public async processOutput<T>(data: any): Promise<T> {
+    if (!data[this.schemaField.name]) {
+      return (null as any) as T;
+    }
+
+    const criteria: { id: string; schemaId: string } = data[this.schemaField.name];
+
+    const document: Document = await this.repositories.Document.createQueryBuilder('document')
+      .leftJoinAndSelect('document.schema', 'schema')
+      .where('document.id = :id', { id: criteria.id })
+      .andWhere('schema.id = :schemaId', { schemaId: criteria.schemaId })
+      .getOne();
+
+    return (document.data as any) as T;
+  }
+}
