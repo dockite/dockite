5305505e7e5230408949cf7a0fe6d4f7045046d9
Mythie <me@lucasjamessmith.me>
2020-06-01 21:57:36 +1000

    chore: remove deprecated ui package

--- packages/ui/src/ambient.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'vue-smooth-dnd';
-declare module 'vue-fragment';
-declare module 'vue-codemirror';
--- packages/ui/src/store/document/mutations.ts
+++
@@ -1,9 +0,0 @@
-import { MutationTree } from 'vuex';
-
-import { DocumentState } from './types';
-
-export const mutations: MutationTree<DocumentState> = {
-  setDocumentId(state, documentId: string) {
-    state.documentId = documentId;
-  },
-};
