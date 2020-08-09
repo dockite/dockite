24324e64fd2511088a25c56e61a08d6b7f3fcfce
Lucas Smith <me@lucasjamessmith.me>
2020-08-09 11:52:38 +1000

    fix(core): remove dockiteField from webhook payloads

--- packages/core/src/utils/fire-webhooks.ts
+++ packages/core/src/utils/fire-webhooks.ts
@@ -8,7 +8,15 @@ import { getRepository, InsertResult } from 'typeorm';
 
 const log = debug('dockite:core:webhooks');
 
-export const fireWebhooks = async (entity: object, action: WebhookAction): Promise<void> => {
+const dockiteFieldReplacer = (key: string, value: any): any => {
+  if (key === 'dockiteField') {
+    return undefined;
+  }
+
+  return value;
+};
+
+export const fireWebhooks = async (entity: any, action: WebhookAction): Promise<void> => {
   const webhookRepository = getRepository(Webhook);
   const webhookCallRepository = getRepository(WebhookCall);
 
@@ -29,7 +37,10 @@ export const fireWebhooks = async (entity: object, action: WebhookAction): Promi
           return Axios({
             url: webhook.url,
             method: webhook.method as Method,
-            data: webhook.method.toLowerCase() === 'get' ? undefined : JSON.stringify(entity),
+            data:
+              webhook.method.toLowerCase() === 'get'
+                ? undefined
+                : JSON.stringify(entity, dockiteFieldReplacer),
           }).then(
             (response: AxiosResponse) => {
               return webhookCallRepository.insert({
@@ -37,7 +48,10 @@ export const fireWebhooks = async (entity: object, action: WebhookAction): Promi
                 request: {
                   url: webhook.url,
                   method: webhook.method,
-                  data: webhook.method.toLowerCase() === 'get' ? undefined : JSON.stringify(entity),
+                  data:
+                    webhook.method.toLowerCase() === 'get'
+                      ? undefined
+                      : JSON.stringify(entity, dockiteFieldReplacer),
                 },
                 response: { headers: response.headers, data: response.data },
                 status: response.status,
@@ -55,7 +69,10 @@ export const fireWebhooks = async (entity: object, action: WebhookAction): Promi
                 request: {
                   url: webhook.url,
                   method: webhook.method,
-                  data: webhook.method.toLowerCase() === 'get' ? undefined : JSON.stringify(entity),
+                  data:
+                    webhook.method.toLowerCase() === 'get'
+                      ? undefined
+                      : JSON.stringify(entity, dockiteFieldReplacer),
                 },
                 response: {
                   headers: error.response?.headers ?? {},
