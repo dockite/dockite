808e7df798ea3c30db01610036ebed175512f642
Lucas Smith <me@lucasjamessmith.me>
2020-09-29 15:57:03 +1000

    fix(field-variant): update children typing

--- packages/field-variant/types.ts
+++ packages/field-variant/types.ts
@@ -1,9 +1,11 @@
 import { BaseField } from '@dockite/database';
 import { FieldSettings } from '@dockite/types';
 
+export type ChildField = Omit<BaseField, 'id' | 'schema' | 'schemaId' | 'dockiteField'>;
+
 export interface VariantFieldSettings extends FieldSettings {
   required: boolean;
-  children: BaseField[];
+  children: ChildField[];
 }
 
 export interface DockiteFieldVariantEntity extends BaseField {
