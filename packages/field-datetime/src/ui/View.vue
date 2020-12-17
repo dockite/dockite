3174b043c6943df090e1c1540f6ebca30f1e2a2d
Lucas Smith <me@lucasjamessmith.me>
2020-12-18 09:47:17 +1100

    feat(field-datetime): add field view for datetimes

---
+++ packages/field-datetime/src/ui/View.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span v-if="date">
+      {{ format(date, 'Pp') }}
+    </span>
+    <i v-else>
+      N/A
+    </i>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { format } from 'date-fns';
+
+import { DockiteFieldDateTimeEntity } from '../types';
+
+@Component({
+  name: 'DateTimeFieldViewComponent',
+})
+export default class DateTimeFieldViewComponent extends Vue {
+  @Prop({ required: true })
+  readonly data!: string | null;
+
+  @Prop({ required: true, type: Object })
+  readonly field!: DockiteFieldDateTimeEntity;
+
+  public format = format;
+
+  get date(): Date | null {
+    if (this.data) {
+      return new Date(this.data);
+    }
+
+    return null;
+  }
+}
+</script>
+
+<style></style>
