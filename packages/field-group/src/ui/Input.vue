e9a07edda0bcdecb4595a8123d9e1599987e7de0
Mythie <me@lucasjamessmith.me>
2021-03-02 11:24:52 +1100

    fix(field-group): assign value during array normalization

--- packages/field-group/src/ui/Input.vue
+++ packages/field-group/src/ui/Input.vue
@@ -291,7 +291,7 @@ export default class GroupFieldInputComponent extends Vue {
       }
 
       if (Array.isArray(this.value)) {
-        this.value.map(v => ({ ...cloneDeep(this.initialFieldData), ...v }));
+        this.fieldData = this.value.map(v => ({ ...cloneDeep(this.initialFieldData), ...v }));
       }
     } else {
       this.fieldData = { ...cloneDeep(this.initialFieldData), ...this.value };
